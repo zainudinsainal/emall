45981dbf5bb4eeaee3050be4fb142824e99ddddb
ylm <yingleiming@outlook.com>
2020-02-04 21:09:02 +0800

    init

---
+++ cc-shop/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>init page</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
